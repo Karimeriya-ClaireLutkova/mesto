0376c8c06ffac240929d87e5f56ccb638f6e4f1f
Svetlana Lutkova <lilissiya@yandex.ru>
2023-03-30 03:49:28 +0500

    Создать js-код сохранения данных в форме

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,33 @@
-let redactor = document.querySelector('.profile__button_edit')
+let redactor = document.querySelector('.profile__button_edit');
 function showForm() {
-  let formEdit = document.querySelector('.popup')
+  let formEdit = document.querySelector('.popup');
   formEdit.style.visibility = 'visible';
 }
 redactor.addEventListener('click', showForm);
 
-let cancel = document.querySelector('.popup__button_close')
+let cancel = document.querySelector('.popup__button_close');
 function closingForm() {
-  let formEdit = document.querySelector('.popup')
+  let formEdit = document.querySelector('.popup');
   formEdit.style.visibility = 'hidden';
 }
 cancel.addEventListener('click', closingForm);
+
+let formElement = document.querySelector('.popup__form');
+let nameInput = formElement.querySelector('.popup__input_type_name');
+let jobInput = formElement.querySelector('.popup__input_type_profession');
+function handleFormSubmit (evt) {
+    evt.preventDefault();
+    nameInput.value;
+    jobInput.value;
+    let profileName = document.querySelector('.profile__name');
+    let profileJob = document.querySelector('.profile__profession');
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
+    
+}
+formElement.addEventListener('submit', handleFormSubmit);
+function closeSaveInfo() {
+  document.querySelector('.popup').style.visibility = 'hidden';
+} 
+formElement.addEventListener('submit', closeSaveInfo);
+
