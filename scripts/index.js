719ba118aa59115b016495984776922279be3f2d
Svetlana Lutkova <lilissiya@yandex.ru>
2023-04-15 11:47:17 +0500

    Фиксация функций открытия, закрытия и удаления карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,10 @@
-const redactor = document.querySelector('.profile__button_edit');
-const adding = document.querySelector('.profile__button_add');
-const profileName = document.querySelector('.profile__name');
-const profileJob = document.querySelector('.profile__profession');
-const elementsPage = document.querySelector('.elements');
-const forms = document.querySelectorAll('.popup');
-console.log(forms);
-const cancel = document.querySelector('.popup__button_close');
-const formElement = document.querySelector('.popup__form');
-const nameInput = formElement.querySelector('.popup__input_type_name');
-const jobInput = formElement.querySelector('.popup__input_type_profession');
-const cardTemplate = document.querySelector('#card').content;
-const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
-
-
 const initialCards = [
   {
-    name: 'Карачаевск',
+    name: 'Карачаево-Черкесcия',
     link: './images/Карачаевск.jpg',
     title: 'Церковь в Карачаевске'
   },
   {
-    name: 'Гора Эльбрус',
-    link: './images/Гора_Эльбрус.jpg',
-    title: 'Степи у Эльбруса'
-  },
-  {
     name: 'Домбай',
     link: './images/Домбай.jpg',
     title: 'Лес на горной территории Домбая'
@@ -40,7 +20,12 @@ const initialCards = [
     title: 'Лес на горной территории Домбая'
   },
   {
-    name: 'Карачаево-Черкесcия',
+    name: 'Гора Эльбрус',
+    link: './images/Гора_Эльбрус.jpg',
+    title: 'Степи у Эльбруса'
+  },
+  {
+    name: 'Карачаевск',
     link: './images/Карачаевск.jpg',
     title: 'Церковь в Карачаевске'
   }
@@ -54,6 +39,26 @@ const cardInfo = initialCards.map(function (item) {
   };
 });
 
+const redactor = document.querySelector('.profile__button_edit');
+const adding = document.querySelector('.profile__button_add');
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__profession');
+const elementsPage = document.querySelector('.elements');
+const formsPopupAll = [];
+const formPopupEdit = document.querySelector('.popup-info');
+const formPopupCard = document.querySelector('.popup-cardNew');
+const cancel = document.querySelectorAll('.popup__button_close');
+const formElement = document.querySelectorAll('.popup__form');
+const formCardEdit = document.querySelector("[name='profile']");
+const formCardNew = document.querySelector("[name='new-card']");
+const nameInput = formCardEdit.querySelector('.popup__input_type_name');
+const jobInput = formCardEdit.querySelector('.popup__input_type_profession');
+const cardTemplate = document.querySelector('#card').content;
+const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
+
+formsPopupAll.push(formPopupEdit, formPopupCard);
+console.log(formsPopupAll);
+
 function render() {
   cardInfo.forEach(showCard);
 }
@@ -64,36 +69,60 @@ function showCard({ name, link, title }) {
   cardElement.querySelector('.element__title').textContent = name;
   cardElement.querySelector('.element__image').src = link;
   cardElement.querySelector('.element__image').alt = title;
-  elementsPage.append(cardElement);
+  elementsPage.prepend(cardElement);
 }
 render();
 
 function showForm(index) {  
-  forms[index].classList.add('popup_opened');
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileJob.textContent;
+  formsPopupAll[index].classList.add('popup_opened');
+  if (index === 0) {
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
+  }
 }
 
-function closingForm() {
-  forms.classList.remove('popup_opened');
-}
-
-function handleFormSubmit (evt) {
+/*function handleFormSubmit (evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-  closingForm();
+  changesSaveForm();
 };
+function closingForm() {
+  forms.classList.remove('popup_opened');
+}
+*/
 
 redactor.addEventListener('click', ()=> showForm(0));
 adding.addEventListener('click', ()=> showForm(1));
-cancel.addEventListener('click', closingForm);
-formElement.addEventListener('submit', handleFormSubmit);
+//cancel.addEventListener('click', closingForm);
+//formElement.addEventListener('submit', handleFormSubmit);
+formElement.forEach(formElementItem => {
+  formElementItem.addEventListener('submit', function(evt){
+    evt.preventDefault();
+    const elemTarget = evt.target;
+    const elemProfile = elemTarget.querySelector('.popup__input_type_name');
+    if (elemProfile !== null) {
+      profileName.textContent = nameInput.value;
+      profileJob.textContent = jobInput.value;
+    };
+    const elemCancel = elemTarget.closest('.popup');
+    elemCancel.classList.remove('popup_opened');
+  });
+});
+
 const deleteCard = elementsPage.querySelectorAll('.element__button_delete');
 deleteCard.forEach(deleteCardItem => {
   deleteCardItem.addEventListener('click', function(evt){
     const elemTarget = evt.target;
     const elemDel = elemTarget.closest('.element');
     elemDel.remove();
+  });
+});
+
+cancel.forEach(canselItem => {
+  canselItem.addEventListener('click', function(evt){
+    const elemTarget = evt.target;
+    const elemCancel = elemTarget.closest('.popup');
+    elemCancel.classList.remove('popup_opened');
   });
 });
