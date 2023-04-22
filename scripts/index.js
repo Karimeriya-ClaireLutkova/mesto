878c4d0843973716bdc9c2e968d77ad562caa9a5
Svetlana Lutkova <lilissiya@yandex.ru>
2023-04-22 13:20:15 +0500

    Фиксация изменений js-кода после ревью

--- scripts/index.js
+++ scripts/index.js
@@ -7,7 +7,6 @@ const editProfilePopup = document.querySelector('.popup_type_profile-info');
 const addingCardPopup = document.querySelector('.popup_type_card-new');
 const viewCardPopup = document.querySelector('.popup_type_image-view');
 const closePopupButtons = document.querySelectorAll('.popup__button_close');
-const listFofmsPopup = document.querySelectorAll('.popup__form');
 const formProfile = document.querySelector("[name='profile']");
 const formCardNew = document.querySelector("[name='new-card']");
 const formCardView = document.querySelector('.popup__container_type_image');
@@ -62,12 +61,7 @@ initialCards.forEach((item) => {
 
 function assambleCard(item, sectionPageCards) {
   const cardElement = createCard(item);
-  cardElement.name = nameCard.value;
-  cardElement.link = linkImageCard.value;
-  cardElement.title = nameCard.value;
   sectionPageCards.prepend(cardElement);
-  nameCard.value = '';
-  linkImageCard.value = '';
 }
 
 function showForm(item) {
@@ -81,55 +75,24 @@ editProfileButton.addEventListener('click', ()=> showForm(editProfilePopup,
 
 addingCardButton.addEventListener('click', ()=> showForm(addingCardPopup));
 
-listFofmsPopup.forEach(listFofmsPopupItem => {
-  listFofmsPopupItem.addEventListener('submit', function(evt){
+formProfile.addEventListener('submit', function(evt){
     evt.preventDefault();
-    const elemTarget = evt.target;
-    const elemPopup = elemTarget.closest('.popup');
-    if (elemPopup.classList === editProfilePopup.classList) {
-      profileName.textContent = nameInput.value;
-      profileJob.textContent = jobInput.value;
-    } else if (elemPopup.classList === addingCardPopup.classList) {
-      const name = nameCard.value;
-      const link = linkImageCard.value;
-      const title = nameCard.value;
-      assambleCard(name, link, title);
-      nameCard.value = '';
-      linkImageCard.value = '';
-    };    
-    showForm(elemPopup);
-  });
-});
-/*
-const deleteCard = sectionPageCards.querySelectorAll('.element__button_delete');
-deleteCard.forEach(deleteCardItem => {
-  deleteCardItem.addEventListener('click', function(evt){
-    const elemTarget = evt.target;
-    const elemDel = elemTarget.closest('.element');
-    elemDel.remove();
-  });
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
+    showForm(editProfilePopup);
 });
 
-const preference = sectionPageCards.querySelectorAll('.element__button_like');
-  preference.forEach(preferenceItem => {
-    preferenceItem.addEventListener('click', function(evt){
-      const elemTarget = evt.target;
-      elemTarget.classList.toggle('element__button_like_active');
-  });
+formCardNew.addEventListener('submit', function(evt){
+  evt.preventDefault();
+  const name = nameCard.value;
+  const link = linkImageCard.value;
+  const title = nameCard.value;
+  assambleCard ({name, link, title}, sectionPageCards);
+  nameCard.value = '';
+  linkImageCard.value = '';
+  showForm(addingCardPopup);
 });
 
-const viewing = sectionPageCards.querySelectorAll('.element__image');
-viewing.forEach(viewingItem => {
-  viewingItem.addEventListener('click', function(evt){
-    const elemTarget = evt.target;
-    const elemView = elemTarget.closest('.element');
-    elemSubtitleView = elemView.querySelector('.element__title');
-    imageCardView.src = elemTarget.src;
-    imageSubtitleView.textContent = elemSubtitleView.textContent;
-    showForm(viewCardPopup);
-  });
-});
-*/
 closePopupButtons.forEach(canselItem => {
   canselItem.addEventListener('click', function(evt){
     const elemTarget = evt.target;
