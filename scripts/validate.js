5e7866c108123cb9da7eebd7eb543e2ebc9e647f
Svetlana Lutkova <lilissiya@yandex.ru>
2023-04-26 17:49:18 +0500

    Реализовать отображения текста ошибки в форме редактирования профиля

--- scripts/validate.js
+++ scripts/validate.js
@@ -2,6 +2,14 @@ const formElement = document.querySelector('.popup__form');
 const formInput = document.querySelector('.popup__input');
 const formError = formElement.querySelector(`.${formInput.id}-error`);
 
+const fillTextContentError = (element) => {
+  formError.textContent = element;
+};
+
+const clearTextContentError = () => {
+  formError.textContent = '';
+};
+
 const showMessageError = () => {
   formError.classList.add('popup__input-error_active');
 };
@@ -10,17 +18,21 @@ const hideMessageError = () => {
   formError.classList.remove('popup__input-error_active');
 }; 
 
-const showInputError = (element) => {
+const showInputError = (element, errorMessage) => {
   element.classList.add('popup__input_type_error');
+  fillTextContentError(errorMessage);
+  showMessageError();
 };
 
 const hideInputError = (element) => {
   element.classList.remove('popup__input_type_error');
+  hideMessageError();
+  clearTextContentError();
 }
 
 const isValid = () => {
   if (!formInput.validity.valid) {
-    showInputError(formInput);
+    showInputError(formInput, formInput.validationMessage);
   } else {
     hideInputError(formInput);
   }
