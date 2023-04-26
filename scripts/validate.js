dd8223269d0dcbf247528f859bbd68d6b7812202
Svetlana Lutkova <lilissiya@yandex.ru>
2023-04-26 18:07:55 +0500

    Настроить стили для span

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,5 +1,14 @@
 const formElement = document.querySelector('.popup__form');
 const formInput = document.querySelector('.popup__input');
+const formError = formElement.querySelector(`.${formInput.id}-error`);
+
+const showMessageError = () => {
+  formError.classList.add('popup__input-error_active');
+};
+
+const hideMessageError = () => {
+  formError.classList.remove('popup__input-error_active');
+}; 
 
 const showInputError = (element) => {
   element.classList.add('popup__input_type_error');
