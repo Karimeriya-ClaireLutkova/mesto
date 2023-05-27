2cdbb7fb6fec608c7a2c766eca5eb8ad99bf8cc7
Svetlana Lutkova <lilissiya@yandex.ru>
2023-05-27 06:42:54 +0500

    Создать class UserInfo

--- scripts/PopupWithForm.js
+++ scripts/PopupWithForm.js
@@ -9,27 +9,27 @@ export default class PopupWithForm extends Popup {
     this._formValues = {};
   }
 
+  _getInputValues() {
+    this._inputList.forEach(input => {
+      this._formValues[input.name] = input.value;
+      console.log(this._formValues)
+    });    
+    console.log(this._formValues);
+    return this._formValues;
+  }
+  
   setEventListeners() {
     super.setEventListeners();
     this._form.addEventListener('submit', function(evt) {
       evt.preventDefault();
-      this._handleFormSubmit(this._getInputValues());
+      this._handleFormSubmit();
     });
-  }
-
-  _getInputValues() {
-    this._inputList.forEach(input => {
-      this._formValues[input.name] = input.value;
-    });
-
-    console.log(this._formValues);
-    return this._formValues;
   }
 
   openPopup() {
     super.openPopup();
   }
-  
+
   closePopup() {
     super.closePopup();
     this._form.reset();
