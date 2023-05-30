37d0589110d57a283eaa8428c7fd24ae8a001f1b
Svetlana Lutkova <lilissiya@yandex.ru>
2023-05-30 11:27:17 +0500

    Исправить в настройках webpack

--- src/scripts/Popup.js
+++ src/scripts/Popup.js
@@ -3,20 +3,17 @@ export default class Popup {
     this._selector = selectorPopup;
     this._popup = document.querySelector(this._selector);
     this._buttonClosePopup = this._popup.querySelector('.popup__button_close');
+    this._handleEscClose = this._handleEscClose.bind(this);
   }
 
   openPopup() {
     this._popup.classList.add('popup_opened');
-    document.addEventListener('keydown', (evt) => {
-      this._handleEscClose(evt);
-    });
+    document.addEventListener('keydown', this._handleEscClose);
   }
 
   closePopup() {
     this._popup.classList.remove('popup_opened');
-    document.removeEventListener('keydown', (evt) => {
-      this._handleEscClose(evt);
-    });
+    document.removeEventListener('keydown', this._handleEscClose);
   }
 
   _handleEscClose(evt) {
